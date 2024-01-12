11a60ac6835238e191b869011c6f3cf1bfbf8420
quy12022002 <ban12022002@gmail.com>
2024-01-12 17:27:20 +0700

    learn state

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -3,15 +3,17 @@
 
 import React from "react";
 
-class MyComponent extends React.Component{
-    // JSX 
-    render(){
-        return (
-            <div>my first component
-                {Math.random()}
-            </div>
-        );
-     }
+class MyComponent extends React.Component {
+  state = { name: "QUY", address: "TDC", age: 26 };
+
+  // JSX
+  render() {
+    return (
+      <div>
+        My name is {this.state.name} and I'm from {this.state.address}
+      </div>
+    );
+  }
 }
 
 export default MyComponent;
