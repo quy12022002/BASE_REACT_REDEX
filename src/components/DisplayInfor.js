c77f2d2768fc0061af548735906bd7b9f722e9f7
quy12022002 <ban12022002@gmail.com>
2024-01-13 18:14:57 +0700

    if else

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,20 +1,41 @@
 import React from "react";
 
 class DisplayInfor extends React.Component {
+  state = {
+    isShowOrHide: false,
+  };
+  handleShowHide = () => {
+    this.setState({
+      isShowOrHide: !this.state.isShowOrHide,
+    });
+  };
+
   render() {
     // destructuring array/object
     const { listUsers } = this.props;
     // props: viet tat properties
-    console.log(this.props);
     return (
       <div>
-        {listUsers.map((item) => {
-          return (
-            <div key={item.id}>
-              My name's {item.name} and my age's {item.age}
-            </div>
-          );
-        })}
+        <div>
+          <span
+            onClick={() => {
+              this.handleShowHide();
+            }}
+          >
+            {this.state.isShowOrHide === true ? "Hide" : "Show"} list user
+          </span>
+        </div>
+        {this.state.isShowOrHide && (
+          <div>
+            {listUsers.map((item) => {
+              return (
+                <div key={item.id} className={item.age <= 16 ? "green" : "red"}>
+                  My name's {item.name} and my age's {item.age}
+                </div>
+              );
+            })}
+          </div>
+        )}
       </div>
     );
   }
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -8,7 +8,7 @@ import DisplayInfor from "./DisplayInfor";
 class MyComponent extends React.Component {
   state = {
     listUsers: [
-      { id: 1, name: "QUY", age: 21 },
+      { id: 1, name: "QUY", age: 16 },
       { id: 2, name: "QUY1", age: 21 },
       { id: 3, name: "QUY2", age: 21 },
     ],
