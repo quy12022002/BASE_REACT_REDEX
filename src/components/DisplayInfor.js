de1e4935207b15327fcf2ef06c743e037d63383e
quy12022002 <ban12022002@gmail.com>
2024-01-17 20:45:45 +0700

    stateless/stateful

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,55 +1,68 @@
 import "./DisplayInfor.scss";
 import React from "react";
 
-class DisplayInfor extends React.Component {
-  state = {
-    isShowOrHide: true,
-  };
-  handleShowHide = () => {
-    this.setState({
-      isShowOrHide: !this.state.isShowOrHide,
-    });
-  };
+// stateless vs stateful
+// class DisplayInfor extends React.Component {
+//   //template + logic js
+//   render() {
+//     // destructuring array/object
+//     const { listUsers } = this.props;
+//     // props: viet tat properties
+//     return (
+//       <div className="display-info-container">
+//         <div>Show list users</div>
+//         {true && (
+//           <>
+//             {listUsers.map((item) => {
+//               return (
+//                 <div
+//                   key={item.id}
+//                   className={+item.age <= 16 ? "green" : "red"}
+//                 >
+//                   <p>
+//                     My name's {item.name} and my age's {item.age}
+//                   </p>
+//                   <button onClick={() => this.props.hanldleDeleteUser(item.id)}>
+//                     Delete
+//                   </button>
+//                   <hr />
+//                 </div>
+//               );
+//             })}
+//           </>
+//         )}
+//       </div>
+//     );
+//   }
+// }
 
+const DisplayInfor = (props) => {
+  // destructuring array/object
+  const { listUsers } = props;
   //template + logic js
-  render() {
-    // destructuring array/object
-    const { listUsers } = this.props;
-    // props: viet tat properties
-    return (
-      <div className="display-info-container">
-        <div>
-          <span
-            onClick={() => {
-              this.handleShowHide();
-            }}
-          >
-            {this.state.isShowOrHide === true ? "Hide" : "Show"} list user
-          </span>
-        </div>
-        {this.state.isShowOrHide && (
-          <>
-            {listUsers.map((item) => {
-              return (
-                <div
-                  key={item.id}
-                  className={+item.age <= 16 ? "green" : "red"}
-                >
-                  <p>
-                    My name's {item.name} and my age's {item.age}
-                  </p>
-                  <button onClick={() => this.props.hanldleDeleteUser(item.id)}>
-                    Delete
-                  </button>
-                  <hr />
-                </div>
-              );
-            })}
-          </>
-        )}
-      </div>
-    );
-  }
-}
+  // props: viet tat properties
+  return (
+    <div className="display-info-container">
+      <div>Show list users</div>
+      {true && (
+        <>
+          {listUsers.map((item) => {
+            return (
+              <div key={item.id} className={+item.age <= 16 ? "green" : "red"}>
+                <p>
+                  My name's {item.name} and my age's {item.age}
+                </p>
+                <button onClick={() => props.hanldleDeleteUser(item.id)}>
+                  Delete
+                </button>
+                <hr />
+              </div>
+            );
+          })}
+        </>
+      )}
+    </div>
+  );
+};
 
 export default DisplayInfor;
